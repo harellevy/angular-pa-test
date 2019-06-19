f50e60d0aa067e941915c990204f147f92267980
harellevy <harellevy@gmail.com>
2019-06-19 18:49:04 +0300

    - implemented simple ip input component instead of using buggy ngx-mask.

--- src/app/features/device-list/device-list.module.ts
+++ src/app/features/device-list/device-list.module.ts
@@ -3,12 +3,8 @@ import { DeviceListComponent } from './device-list.component';
 import { DeviceService } from './device.service';
 import { DeviceFormComponent } from './device-form/device-form.component';
 import { SharedModule } from '../shared/shared.module';
-import { IConfig, NgxMaskModule } from 'ngx-mask';
 import { IpInputComponent } from './device-form/ip-input/ip-input.component';
 import { DeviceListItemComponent } from './device-list-item/device-list-item.component';
-
-export let ngxMastOptions: Partial<IConfig> | (() => Partial<IConfig>);
-
 
 @NgModule({
   declarations: [
@@ -22,7 +18,6 @@ export let ngxMastOptions: Partial<IConfig> | (() => Partial<IConfig>);
   ],
   imports: [
     SharedModule,
-    NgxMaskModule.forRoot(ngxMastOptions)
   ],
   exports: [
     DeviceListComponent
